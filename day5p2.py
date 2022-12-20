c95032fceef8b2521d13b3706118800d57be0ad9
Ingri Benedicte Rønning <ingri.benedicte.ronning@spk.no>
2022-12-20 08:44:55 +0100

    Added first days

---
+++ day5p2.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+input = Path.read_text(Path('input/day5.txt')).split('\n')
+ind = input.index('')
+n_stacks = int(str.split(input[ind - 1], ' ')[-1])
+indices = [0] + [(3 + 4*i) for i in range(n_stacks)]
+
+stacks_horizontal, stacks = [], [[] for i in range(n_stacks)]
+for i in range(len(input[:ind])):
+    stacks_horizontal.append([input[i][j:k].strip() for j, k in zip(indices[:-1], indices[1:])])
+
+for i in range(n_stacks):
+    for j in reversed(range(len(stacks_horizontal)-1)):
+        if stacks_horizontal[j][i] != '':
+            stacks[i].append(stacks_horizontal[j][i])
+
+for step in input[ind+1:]:
+    step = str.split(step, ' ')
+    n, f, t = int(step[1]), int(step[3]), int(step[5])
+    chunk = []
+    for i in range(n):
+        chunk.insert(0, stacks[f-1].pop())
+    stacks[t-1].extend(chunk)
+
+top_crates = "".join([stack[-1][1] for stack in stacks])
+print(f"Answer Part Two: {top_crates}")
