6c1ca926759a1e82c8695d15d8a150b3b14d832a
Ingri Benedicte Rønning <ingri.benedicte.ronning@spk.no>
2022-12-21 11:55:04 +0100

    Added day 20 part two

--- day20.py
+++ day20.py
@@ -1,12 +1,10 @@
 from pathlib import Path
 
-numbers, orders, indices = [], [], []
-for i, line in enumerate(Path.read_text(Path('input/day20.txt')).strip().split('\n')):
-    numbers.append(int(line))
-    indices.append(i)
+numbers = [int(line) for line in Path.read_text(Path('input/day20.txt')).strip().split('\n')]
+n = len(numbers)
+indices = [i for i in range(n)]
 
-def mixing(numbers, indices):
-    sequence = numbers.copy()
+def mixing(sequence, numbers, indices):
     max_index = max(indices)
 
     j = 0
@@ -40,12 +38,19 @@ def mixing(numbers, indices):
         j += 1
     return sequence, indices
 
-sequence, post_indices = mixing(numbers, indices)
+sequence = numbers.copy()
+mixed_sequence, mixed_indices = mixing(sequence, numbers, indices)
+starting_index = mixed_sequence.index(0)
+i_1000, i_2000, i_3000 = (starting_index + 1000) % n, (starting_index + 2000) % n, (starting_index + 3000) % n
+print(f"Answer Part One: {mixed_sequence[i_1000] + mixed_sequence[i_2000] + mixed_sequence[i_3000]}")
+
+decrypted_numbers = [(number * 811589153) for number in numbers]
+sequence = decrypted_numbers.copy()
+indices = [i for i in range(len(decrypted_numbers))]
+for i in range(10):
+    sequence, post_mix_indices = mixing(sequence, decrypted_numbers, indices)
+    indices = post_mix_indices.copy()
+
 starting_index = sequence.index(0)
-n = len(sequence)
-
-i_1000 = (starting_index + 1000) % n
-i_2000 = (starting_index + 2000) % n
-i_3000 = (starting_index + 3000) % n
-
-print(f"Answer Part One: {sequence[i_1000] + sequence[i_2000] + sequence[i_3000]}")
+i_1000, i_2000, i_3000 = (starting_index + 1000) % n, (starting_index + 2000) % n, (starting_index + 3000) % n
+print(f"Answer Part Two: {sequence[i_1000] + sequence[i_2000] + sequence[i_3000]}")
